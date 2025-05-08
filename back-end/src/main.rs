c77d0870d143007a4c5dfcc4293115b16c3cbf73
phanidharguttikonda0 <phanidharguttikonda0@gmail.com>
2025-05-08 21:20:15 +0530

    added new routes(for players)

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -5,8 +5,9 @@ mod graph_ql_fields;
 mod auction_room;
 
 
-use async_graphql::{EmptyMutation, EmptySubscription, Schema, SchemaBuilder};
-use tracing_subscriber::{fmt, EnvFilter};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use handlers::players::{get_player, get_stats};
+use tracing_subscriber::EnvFilter;
 
 
 use axum::{Extension, Router, extract::Path};
@@ -17,9 +18,8 @@ use crate::handlers::profile::{get_profile, profile, reset_password, search};
 use crate::handlers::room_handler::{get_pool, get_public_rooms, get_remaining_teams, get_team, get_teams};
 use crate::middlewares::authentication::{authorization_check, validate_details};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use tracing_subscriber::fmt::layer;
 use crate::auction_room::handle_ws_upgrade;
-use crate::graph_ql_fields::{QueryRoot, User};
+use crate::graph_ql_fields::QueryRoot;
 
 fn init_tracing() {
     tracing_subscriber::fmt()
@@ -60,9 +60,14 @@ fn profile_routes() -> Router<AppState> {
     Router::new().route("/", get(profile).layer(middleware::from_fn(authorization_check)))
         .route("/search/{username}", get(search))
         .route("/reset-password", post(reset_password).layer(middleware::from_fn(authorization_check)))
-        .route("/get-user/{username}", get(get_profile))
+        .route("/get-user/{username}", get(get_profile)) // returns room-id's along with data played and username mail-id etc
 }
 
+fn player_routes() -> Router<AppState> {
+    Router::new().route("/get-player/:player_id", get(get_player))
+    .route("/get-stats/:stats_id", get(get_stats))
+    .layer(middleware::from_fn(authorization_check))
+}
 
 async fn graphql_handler(Path(room_id): Path<String>, schema: Extension<Schema<QueryRoot, EmptyMutation, EmptySubscription>>, req: GraphQLRequest) -> GraphQLResponse {
     let ctx = room_id ;
@@ -88,9 +93,10 @@ async fn main() {
     let schema = Schema::build(QueryRoot,EmptyMutation,EmptySubscription).finish() ;
     let app = Router::new()
         .nest("/authentication", authentication_routes())
-        .nest("/room", room_routes())
+        .nest("/rooms", room_routes())
         .nest("/user", profile_routes())
         .route("/", get(server_start))
+        .nest("/player", player_routes())
         .route("/ws", get(handle_ws_upgrade))
         .route("/graphql/{room_id}", post(graphql_handler))
         .with_state(state)
