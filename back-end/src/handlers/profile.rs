ebc9a4ede8c29cd905e72e0de5b7158855f1f92e
phanidharguttikonda0 <phanidharguttikonda0@gmail.com>
2025-05-14 10:07:21 +0530

    written logic for search user

--- back-end/src/handlers/profile.rs
+++ back-end/src/handlers/profile.rs
@@ -9,11 +9,27 @@ pub async fn profile(State(state): State<crate::AppState>,req: Request) -> Json<
     Json(get_user_profile("".to_string(), 0).await)
 }
 
-pub async fn search(State(state): State<crate::AppState>, Path(username): Path<String>) -> Json<Vec<(String, i32)>> {
-    Json(vec![]) // where username and user-id we are going to return
+pub async fn search(State(state): State<crate::AppState>, Path(username): Path<String>) -> Json<Result<Vec<(String, i32)>, String>> {
+    let pattern = format!("{}%", username) ;
+    let usernames = sqlx::query_as::<_,(String, i32)>("select username, id from users where username LIKE ($1) LIMIT 5")
+        .bind(pattern).fetch_all(&state.sql_database).await ;
+
+    match usernames {
+        Ok(users) => {
+            tracing::info!("got the usernames for users starting with usernames {}",username) ;
+            Json(Ok(users))
+        },
+        Err(err) => {
+            tracing::error!("error was {}",err) ;
+            Json(Err(String::from("No user found")))
+        }
+    }
+
+
 }
 
 pub async fn reset_password(State(state): State<crate::AppState>, Form(password):Form<Password>) -> Json<bool> {
+    // passing direct new password
     Json(true)
 }
 
