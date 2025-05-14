927b7d8da8a9b4f5be08812092e78c781bd4e6ac
phanidharguttikonda0 <phanidharguttikonda0@gmail.com>
2025-05-14 10:05:54 +0530

    written logic for getting players

--- back-end/src/handlers/players.rs
+++ back-end/src/handlers/players.rs
@@ -1,13 +1,41 @@
 
-use axum::extract::{State,Path};
+use axum::extract::{State, Path};
 use axum::Json;
 use crate::AppState;
 use crate::models::players::{Player, Stats};
 
-pub async fn get_player(State(state): State<AppState>, Path(player_id): Path<i32>) -> Json<Vec<Player>> {
-    Json(vec![])
+pub async fn get_player(State(state): State<AppState>, Path(player_id): Path<i32>) -> Json<Result<Player,String>> {
+
+    let player = sqlx::query_as::<_,Player>("select * from players where id=($1)")
+        .bind(player_id).fetch_one(&state.sql_database).await ;
+
+    match player {
+        Ok(player) => {
+            tracing::info!("got the player for the give player_id {}", player_id) ;
+            Json(Ok(player))
+        },
+        Err(err) => {
+            tracing::error!("error was {}", err) ;
+            Json(Err(String::from("Invalid player-id")))
+        }
+    }
+
 }
 
-pub async fn get_stats(State(state): State<AppState>, Path(stats_id): Path<i32>) -> Json<Vec<Stats>> {
-    Json(vec![])
+pub async fn get_stats(State(state): State<AppState>, Path(stats_id): Path<i32>) -> Json<Result<Stats,String>> {
+    
+    let stats = sqlx::query_as::<_,Stats>("select * from stats where id=($1)")
+        .bind(stats_id).fetch_one(&state.sql_database).await ;
+    
+    match stats { 
+        Ok(stats) => {
+            tracing::info!("got the stats for the given stats_id {}", stats_id) ;
+            Json(Ok(stats))
+        },
+        Err(err) => {
+            tracing::error!("error was {}", err) ;
+            Json(Err(String::from("invalid stats-id")))
+        }
+    }
+
 }
